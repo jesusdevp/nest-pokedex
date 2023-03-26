694bbf87d705024c1846adf54f62835e841d3bfc
Jesus Ramirez <jesusdevelp@gmail.com>
2023-03-25 20:39:13 -0600

    Pokedex v1

---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { ParsMongoIdPipe } from '../common/pipes/pars-mongo-id/pars-mongo-id.pipe';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly pokemonService: PokemonService) {}
+
+  @Post()
+  // @HttpCode(HttpStatus.OK)
+  create(@Body() createPokemonDto: CreatePokemonDto) {
+    return this.pokemonService.create(createPokemonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
+  }
+
+  @Patch(':term')
+  update(@Param('term') term: string, @Body() updatePokemonDto: UpdatePokemonDto) {
+    return this.pokemonService.update(term, updatePokemonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParsMongoIdPipe) id: string) {
+    return this.pokemonService.remove(id);
+  }
+}
